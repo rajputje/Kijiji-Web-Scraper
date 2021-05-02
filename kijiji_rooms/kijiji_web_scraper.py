06bfea3a572600970cbbcf76820134b9ad054347
Jeet Rajput <jeetrajput117@gmail.com>
2021-05-02 19:59:21 -0400

    Added Bootstrap, some refactoring

--- kijiji_rooms/kijiji_web_scraper.py
+++ kijiji_rooms/kijiji_web_scraper.py
@@ -1,54 +1,74 @@
 import requests
 import re
 
+
 from bs4 import BeautifulSoup
 
-
-pageNum = 1
 
 minPrice = 450
 
 maxPrice = 700
 
 
-url='https://www.kijiji.ca/b-for-rent/mississauga-peel-region/room/page-' + str(pageNum) + 'k0c30349001l1700276?rb=true&ll=43.733835%2C-79.823181&address=11965+Hurontario+Street%2C+Brampton%2C+ON&ad=offering&radius=11.0&dc=true'
-
-response = requests.get(url)
-html = BeautifulSoup(response.text, 'html.parser')
-
-
-infoList = html.findAll('div', class_='search-item')
-
 class Room:
     price = ''
     title = ''
 
+
     link = ''
     distance = ''
+
     imageUrl = ''
 
-rooms = []
 
-for info in infoList:
+def searchRooms(pageNum):
 
-    room = Room()
+    url='https://www.kijiji.ca/b-for-rent/mississauga-peel-region/room/page-' + str(pageNum) + '/k0c30349001l1700276?rb=true&ll=43.733835%2C-79.823181&address=11965+Hurontario+Street%2C+Brampton%2C+ON&ad=offering&radius=11.0&dc=true'
 
-    room.price = info.find('div', class_='price').string.strip()
+    response = requests.get(url)
 
-    if re.search('[a-zA-Z]', room.price) == None:
+    html = BeautifulSoup(response.text, 'html.parser')
 
-        priceFloat = float(room.price.strip('$').replace(',', ''))
 
-        if not(minPrice <= priceFloat <= maxPrice):
-            continue
+    infoList = html.findAll('div', class_='search-item')
 
-    room.title = info.find('a', class_='title').string.strip()
 
-    room.link = info.find('a', class_='title').get('href')
+    rooms = []
 
-    room.distance = info.find('div', class_='distance').string.strip()
-    room.imageUrl = info.find('img').get('data-src', None)
-    if room.imageUrl == None:
-        room.imageUrl = info.find('img')['src']
-        
-    rooms.append(room)
+
+    for info in infoList:
+
+
+        room = Room()
+
+
+        room.price = info.find('div', class_='price').string.strip()
+
+
+        if re.search('[a-zA-Z]', room.price) == None:
+
+
+            priceFloat = float(room.price.strip('$').replace(',', ''))
+
+
+            if not(minPrice <= priceFloat <= maxPrice):
+                continue
+
+
+        room.title = info.find('a', class_='title').string.strip()
+
+
+        room.link = info.find('a', class_='title').get('href')
+
+
+        room.distance = info.find('div', class_='distance').string.strip()
+
+        room.imageUrl = info.find('img').get('data-src', None)
+
+        if room.imageUrl == None:
+
+            room.imageUrl = info.find('img')['src']
+            
+        rooms.append(room)
+
+    return rooms
--- kijiji_rooms/main.py
+++ kijiji_rooms/main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+
 import kijiji_web_scraper
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def home():
-    return render_template("home.html", rooms = kijiji_web_scraper.rooms)
+@app.route("/<pageNum>")
+
+def home(pageNum = 1):
+
+    rooms = kijiji_web_scraper.searchRooms(pageNum)
+
+    return render_template("home.html", pageNum = pageNum, rooms = rooms)
+
 
 if __name__ == "__main__":
+
     app.run(debug=True)
