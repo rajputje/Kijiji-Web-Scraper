cb5b095c16ed65548fc5baef4041d9a6ea3d397a
Jeet Rajput <jeetrajput117@gmail.com>
2021-08-12 21:51:01 -0400

    Removed some unused code

--- kijiji_rooms/static/js/autocomplete.js
+++ kijiji_rooms/static/js/autocomplete.js
@@ -8,21 +8,4 @@ function initAutoComplete(){
             fields: ['name']
         }
     );
-
-    //autocompleteLocation.addListener('place_changed', onPlaceChanged);
 }
-
-/*
-function onPlaceChanged(){
-    console.log("on place changed");
-    var place = autocompleteLocation.getPlace();
-
-    if(!place.geomoetry){
-        //User did not select a prediction
-
-    }
-    else{
-
-    }
-}
-*/
