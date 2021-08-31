7a3e490727c4e95aa0b2a3478481d87e090b00b7
Jeet Rajput <jeetrajput117@gmail.com>
2021-08-31 01:04:14 -0400

    Updated urls, added comments

--- kijiji_rooms/static/js/pagination.js
+++ kijiji_rooms/static/js/pagination.js
@@ -1,3 +1,10 @@
+/**
+ * Returns a list item that links to a page number.
+ * @param {string} href Link to navigate to when this item is clicked
+ * @param {*} text Text to be displayed on this item
+ * @param {boolean} isActive Is this the current page?
+ * @returns 
+ */
 function makePageItem(href, text, isActive){
     var li = document.createElement("li");
     li.className = "page-item";
@@ -14,23 +21,38 @@ function makePageItem(href, text, isActive){
   /**
    * Appends page links to container. Page number cannot be below 1.
    * @param {Number} currentPage Current page number
+   * @param {Object} searchVars Variables for filtering search results 
    * @param {Number} numOfPages Number of pages to link
    * @param {Object} container HTML element that will contain the links
    * */
-  function paginate(currentPage, numOfPages, container){
+  function paginate(currentPage, searchVars, numOfPages, container){
     var ul = container;
     var minPage = Math.max(currentPage - 2, 1);
     var maxPage = minPage + numOfPages;
-  
-    var prevHref = (currentPage == 1) ? "/" : "/" + (currentPage-1);
+    searchVars = JSON.parse(searchVars);
+
+    //"Previous" Button
+    var prevHref = (currentPage == 1) ? getUrl(1, searchVars) : getUrl(currentPage-1, searchVars);
     var prevList = makePageItem(prevHref, "Previous", false);
     ul.append(prevList);
 
+    //Rest of the numbered buttons (eg. 1,2,3,4,...)
     for(var i = minPage; i < maxPage; i++){
-      var pageItem = makePageItem("/" + i, i, i == currentPage);
+      var pageItem = makePageItem(getUrl(i, searchVars), i, i == currentPage);
       ul.append(pageItem);
     }
 
-    var nextList = makePageItem("/" + (parseInt(currentPage) + 1), "Next", false);
+    //"Next" button
+    var nextList = makePageItem(getUrl(parseInt(currentPage)+1, searchVars), "Next", false);
     ul.append(nextList);
+}
+
+function getUrl(pageNum, searchVars){
+  href = "/" + pageNum + "?location=" + searchVars.location + "&distance=" + searchVars.distance + "&min_price=" + searchVars.minPrice
+            + "&max_price=" + searchVars.maxPrice;
+  if(searchVars.minPriceEnabled)
+    href += "&min_price_chk=" + searchVars.minPriceEnabled;
+  if(searchVars.maxPriceEnabled)
+    href += "&max_price_chk=" + searchVars.maxPriceEnabled;
+  return href;
 }
